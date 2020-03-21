328d15b32409fe0cd2c6679d759e1878a983b888
Fa <542774602@qq.com>
2020-03-21 22:11:53 +0800

    [+]滚屏致谢界面

---
+++ Assets/Scripts/GameScene_Scripts/WinGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinGame : MonoBehaviour
+{
+    public GameObject go;
+    public Button Btn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Btn.onClick.AddListener(Back_lose);
+        Invoke("SetInfoShow",3);
+    }
+
+    private void SetInfoShow()
+    {
+        this.gameObject.SetActive(false);
+        go.gameObject.SetActive(true);
+    }
+
+    public void Back_lose(){
+        SceneManager.LoadScene("main");
+    }
+}
---
+++ Assets/Scripts/OtherControllers_Scripts/TestScroll.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScroll : MonoBehaviour
+{
+    //设置ScrollRect变量
+
+    ScrollRect rect;
+
+    void Start()
+    {
+        //获取 ScrollRect变量
+        rect = this.GetComponent<ScrollRect>();
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButton(0))
+            return;
+        //在FixedUpdate函数中调用ScrollValue函数
+        ScrollValue();
+    }
+
+    private void ScrollValue()
+    {
+        //当对应值超过1，重新开始从 0 开始
+        if (rect.verticalNormalizedPosition <= 0.0f)
+        
+        {
+            rect.verticalNormalizedPosition = 1.0f;
+        }
+
+        //逐渐递增 ScrollRect 水平方向上的值
+        rect.verticalNormalizedPosition = rect.verticalNormalizedPosition - 0.05f * Time.deltaTime;
+    }
+}
